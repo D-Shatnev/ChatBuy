eff92c0a61a679ba247324eea647b5440a0a67a8
Dmitrii Shatnev <d.shatnev@inventos.ru>
2024-05-16 21:57:06 +0300

    feat: Update consultant agent
    - Add method for advise user

--- chat_buy/agents/consultant_agent.py
+++ chat_buy/agents/consultant_agent.py
@@ -2,8 +2,29 @@
 This module contains the ConsultantAgent class.
 """
 
+from typing import AsyncGenerator
 from chat_buy.agents.base_agent import BaseAgent
 
 
 class ConsultantAgent(BaseAgent):
-    pass
+    """
+    An agent providing advice to the user.
+    """
+
+    async def advise_user(self, messages: list) -> AsyncGenerator[str, None]:
+        """
+        Asynchronously provides advice to the user based on his messages.
+
+        Args:
+            messages (list): List of messages.
+
+        Yields:
+            Iterator[AsyncGenerator[str, None]]: A generator that yields the response.
+        """
+        context_msgs = [{"role": "system", "content": self.prompt}] + messages[:]
+        async for part in self.openai_client.get_stream_response(
+            model=self.model,
+            temperature=self.temperature,
+            context_messages=context_msgs,
+        ):
+            yield part
