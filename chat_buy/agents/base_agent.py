4ee87a629dd0c2eaf949849a78f2f08105aa60a7
Dmitrii Shatnev <d.shatnev@inventos.ru>
2024-05-16 21:41:16 +0300

    feat: Update base agent
    - Extend base init
    - Optimize code

--- chat_buy/agents/base_agent.py
+++ chat_buy/agents/base_agent.py
@@ -7,7 +7,8 @@ from abc import ABC
 from pathlib import Path
 from typing import Any
 
-from chat_buy.constants import AGENT_CONFIG
+from chat_buy.constants import AGENT_CONFIG, API_BASE, API_KEY
+from chat_buy.openai import OpenAIClient
 
 
 class BaseAgent(ABC):
@@ -18,13 +19,14 @@ class BaseAgent(ABC):
     def __init__(self) -> None:
         config = self.load_config(AGENT_CONFIG)
         class_name = self.__class__.__name__
-        if class_name in config:
-            params = config[class_name]
-            self.prompt = params.get("prompt", "You are a helpful assistant.")
-            self.model = params.get("model", "gpt-4-turbo")
-            self.temperature = params.get("temperature", 0.5)
-        else:
+        if class_name not in config:
             raise ValueError(f"Agent {class_name} not found in config file.")
+
+        params = config[class_name]
+        self.prompt = params.get("prompt", "You are a helpful assistant.")
+        self.model = params.get("model", "gpt-4-turbo")
+        self.temperature = params.get("temperature", 0.5)
+        self.openai_client = OpenAIClient(api_base=API_BASE, api_key=API_KEY)
 
     @staticmethod
     def load_config(config_path: Path) -> Any:
--- chat_buy/agents/dialog_control_agent.py
+++ chat_buy/agents/dialog_control_agent.py
@@ -4,18 +4,12 @@ This module contains the DialogControlAgent class.
 
 from chat_buy.agents.base_agent import BaseAgent
 from chat_buy.agents.models import DialogStatus
-from chat_buy.constants import API_BASE, API_KEY
-from chat_buy.openai.openai_client import OpenAIClient
 
 
 class DialogControlAgent(BaseAgent):
     """
     An agent controlling the process of dialog with the user.
     """
-
-    def __init__(self) -> None:
-        super().__init__()
-        self.openai_client = OpenAIClient(api_base=API_BASE, api_key=API_KEY)
 
     async def check_dialog_or_offer(self, messages: list) -> DialogStatus:
         """
@@ -27,14 +21,15 @@ class DialogControlAgent(BaseAgent):
         Returns:
             DialogStatus: The dialog or offer status.
         """
+        context_msgs = [{"role": "system", "content": self.prompt}] + messages[:]
         full_response = []
         async for part in self.openai_client.get_stream_response(
             model=self.model,
             temperature=self.temperature,
-            context_messages=messages,
+            context_messages=context_msgs,
         ):
             full_response.append(part)
             if "<start_search> " in part.lower():
-                return "offer"
+                return DialogStatus.OFFER
         full_response = "".join(full_response)
         return DialogStatus.OFFER if "<start_search> " in full_response.lower() else DialogStatus.DIALOG
